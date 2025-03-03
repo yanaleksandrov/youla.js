8f8ced7a29cd84a94e300de179e9d62b3dbd78b7
yanal <yanalexandrov@gmail.com>
2025-03-03 21:10:43 +0300

    Bulk fixes

--- src/scripts/directives/x-hide.js
+++ src/scripts/directives/x-hide.js
@@ -1,6 +1,5 @@
 import { directive } from '../directives';
 
 directive('hide', (el, expression, attribute, x, component) => {
-  el.style.display = 'none';
-  el.removeAttribute('x-hide');
+  el.style.display = expression ? 'block' : 'none'
 });

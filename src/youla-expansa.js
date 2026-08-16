683f11f4ad6b2ae9c6e081cbb32875c27e517bc0
yanal <yanalexandrov@gmail.com>
2026-08-16 18:00:20 +0300

    Add avatar uploader extension

--- src/youla-expansa.js
+++ src/youla-expansa.js
@@ -13,47 +13,35 @@ document.addEventListener('youla:init', ()=> {
     picture: {
       ':style': "image && `background-image:url(${image})`",
     },
-    initials(letters = 2) {
-      return {
-        'v-show': '!image',
-        'v-text': `name && getInitials(name, ${letters})`,
-      }
+    initials: {
+      'v-show': '!image',
+      'v-text': `name && getInitials(name)`,
     },
     uploader: {
       '@change': 'add($event)',
     },
     remover: {
-      '@click': 'remove($event)',
+      '@click': 'remove($root)',
       'v-show': 'image',
     },
     add(event, callback) {
       let file = event.target.files[0];
       if (file) {
         let reader = new FileReader();
-        reader.onload = e => {
-          this.image = e.target.result;
-        };
+        reader.onload = e => this.image = e.target.result;
         reader.readAsDataURL(file);
       }
-
-      if (callback) {
-        callback();
-      }
+      callback?.();
     },
-    remove(event) {
-      let root  = event.target.closest('[v-data]'),
-        input = root && root.querySelector('input[type="file"]');
+    remove(root) {
+      let input = root.querySelector('input[type="file"]');
       if (input) {
         input.value = '';
       }
       this.image = '';
     },
     getInitials(string, letters = 2) {
-      const wordArray = string.split(' ').slice(0, letters);
-      if (wordArray.length >= 2) {
-        return wordArray.reduce((accumulator, currentValue) => `${accumulator}${currentValue[0].charAt(0)}`.toUpperCase(), '');
-      }
-      return wordArray[0].charAt(0).toUpperCase();
+      return string.split(' ', letters).map(word => word.charAt(0)).join('').toUpperCase();
     },
   }))
 });
